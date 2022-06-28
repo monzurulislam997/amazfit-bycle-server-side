57eba0500d9c7e0add316a703829b4b928fb32ec
fname lname <monzurulislam997@gmail.com>
2022-06-29 01:24:00 +0600

    server is running

--- index.js
+++ index.js
@@ -1,12 +1,62 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
+
+
+
+
+
+const uri = "mongodb+srv://amazfit:gYmjEhvDR71IefnY@cluster0.1u9t2.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+
+    try {
+        await client.connect();
+        const bicycleCollection = client.db("amazfit").collection("products");
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = bicycleCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products)
+        });
+
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+
+            const query = { _id: ObjectId(id) }
+            const products = await bicycleCollection.findOne(query)
+            res.send(products)
+        });
+
+
+    }
+
+    finally {
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
 app.get('/', (req, res) => {
     res.send("Hello amazfit")
 })
+
+
+run().catch(console.dir)
 
 app.listen(port, () => {
     console.log('server is working')
